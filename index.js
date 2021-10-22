4c36973124cd1669cff23a552833cc95d1dfa391
Maria <54138760+mainuniverse@users.noreply.github.com>
2021-10-22 20:12:09 +0300

    add js

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
+const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
+
+const btnStart = document.querySelector('[data-action="start"]');
+const btnStop = document.querySelector('[data-action="stop"]');
+const body = document.querySelector("body");
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+let stop;
+const switchColor = () => {
+  stop = setInterval(() => {
+    let arrayElement = randomIntegerFromInterval(0, colors.length - 1);
+    body.style.backgroundColor = colors[arrayElement];
+  }, 500);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+};
+const stopSwitch = () => {
+  console.log("stopSwitch");
+  clearInterval(stop);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+};
+
+
+btnStart.addEventListener('click', switchColor);
+btnStop.addEventListener('click', stopSwitch);
+
+//   start() {
+//     if (this.isActive) {
+//       return;
+//     }
+//     this.isActive = true;
+//     this.changeBackground = setInterval(() => {
+//       const randomColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+//       document.body.style.background = randomColor;
+//     }, 300);
+//     btnStart.disabled = true;
+//   },
+
+//   stop() {
+//     this.isActive = false;
+//     clearInterval(this.changeBackground);
+//   },
+// };
+// const buttons = {
+//   changeBackground: null,
+//   isActive: false,
